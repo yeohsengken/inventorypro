6df7bc7bb93a7c96dcd810bc1694f53d9de90404
InventoryPro Dev <dev@inventorypro.com>
2026-04-30 07:31:43 +0000

    Added register page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
+  supabaseUrl: 'https://ffjukwdtxsikytirdecj.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmanVrd2R0eHNpa3l0aXJkZWNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MjczMDUsImV4cCI6MjA5MzEwMzMwNX0.-wLbqLut_mvh3IWmuIbjHtZGNyci0xxOXFPtC3QxXDE',
   appName: 'InventoryPro'
 };
